bf68c8c465f54d374542cec18f179d0cd67a83fd
Otaviano Cruz Gouveia de Arruda Neto <58171774+0t4v14n0@users.noreply.github.com>
2021-01-23 04:35:19 -0300

    Add files via upload
    1 - Design a homepage that:
    • Receive the user's name, surname and email as input. • Test whether the email is valid. For this, there can be no blank space in the email address and the presence of @ is required. However, @ cannot be the last element of the address.
    • If all selected tests are correct, enter the name and email in a text file.

--- tratamento.php
+++ tratamento.php
@@ -1,67 +1,27 @@
 <?php
-	$fra=$_POST["ifra"];$ppala=$_POST["i1pala"];$spala=$_POST["i2pala"];
-	$tms=strlen($fra);$tm=strlen($ppala);$t=strlen($spala);
-	$quant = 0;$pala=1;$quanti = 0;$cont = 0;
 
-	for ($i=0; $i <= $tms; $i++) { 
-		$letra = substr($fra, $i, 1);
-		if(($letra == 'a') or ($letra == 'A') or ($letra == 'e') or ($letra == 'E') or ($letra == 'i') or ($letra == 'I') or ($letra == 'o') or ($letra == 'O') or ($letra == 'u') or ($letra == 'U')){
-			$quanti++;$cont++;
-			echo "$letra";
-		}
-		else if($letra == ' '){
-			$arr[$pala-1] = $cont;
-			$pala++;
-		}
-		else{
-			$cont++;
-			echo "$letra";
-		}
+	$nome =$_POST["inome"];  $ar   = 0;
+	$snome=$_POST["isnome"]; $cont = 0;
+	$email=$_POST["iemail"]; $qce  =strlen($email);
+
+	teste($cont,$qce,$email,$ar);
+
+	$letra = substr($email, $qce-1, 1);
+	if((strcasecmp ('@', $letra) == 0) or ($cont > 0) or ($ar > 1)){
+		include('index.html');
+	}
+	else{
+		$txt = fopen("arq.txt", "a");
+		$write = fwrite($txt, "\r\n");
+		$write = fwrite($txt, "Nome : $nome $snome E-mail : $email");
+		fclose($txt);
 	}
 
-	echo "<br/> Quantidade de palavras = $pala <br/>";
-	echo "Quantidade de vogais = $quanti <br/>";
-
-	for ($i=0; $i <= $pala-2; $i++) {
-		if(substr($fra, 0, $arr[$i]) == substr($ppala, 0, $tm)){
-			$lee = substr($fra, 0, $arr[$i]);
-			echo "$lee $spala";
+	function teste(&$cont,$qce,$email,&$ar){
+		for ($i=0; $i <= $qce; $i++) {
+			$a = substr($email, $i, 1);
+			if($a == ' ')$cont++;
+			else if ((substr_compare('@', $a, 1) == 0)) $ar++;
 		}
-	}
-	echo "<br/>";
-
-	for ($i=0; $i <= $tms; $i++) { 
-		$letra = substr($fra, $i, 1);
-		if (($letra == 'a') or ($letra == 'A')){echo "A";}
-		else if (($letra == 'e') or ($letra == 'E')){echo "E";}
-		else if (($letra == 'i') or ($letra == 'I')){echo "I";}
-		else if (($letra == 'o') or ($letra == 'O')){echo "O";}
-		else if (($letra == 'u') or ($letra == 'U')){echo "U";}
-		else{echo "$letra";}
-	}
-
-	echo "<br/>";
-
-	for ($i=0; $i <= $t; $i++) { 
-		$letra = substr($spala, $i, 1);
-		echo "$letra<br/>";
-	}
-
-
-	for ($i=0; $i <= $tms; $i++) { 
-		$letra = substr($fra, $i, 1);
-		if(($letra == 'a') or ($letra == 'A') or ($letra == 'e') or ($letra == 'E') or ($letra == 'i') or ($letra == 'I') or ($letra == 'o') or ($letra == 'O') or ($letra == 'u') or ($letra == 'U')){
-			echo "<span style='color:red;'>" . $letra . "<span/>";
-		}
-		else{
-			echo "<span style='color:black;'>" . $letra . "<span/>" ;
-		}
-	}
-
-	echo "<br/>";
-
-	for ($i=$tm; $i >= 0; $i--) { 
-		$letra = substr($ppala, $i, 1);
-		echo "$letra";
 	}
 ?>

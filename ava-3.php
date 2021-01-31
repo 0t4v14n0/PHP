802be4f3006166c1f407c817219a1c34739318a4
Otaviano Cruz Gouveia de Arruda Neto <58171774+0t4v14n0@users.noreply.github.com>
2021-01-31 04:37:21 -0300

    Add files via upload
    3 – João comprou duas bolas e gostaria de saber se são ou não oficiais. Por favor,  desenvolva um sistema que possa informá-lo. Sabendo que: 
    ● bola de futebol oficial tem peso não inferior a 410g e não superior a 450g 
    ● bola de vôlei oficial tem peso não inferior a 185g e não superior a 240g 
    ● bola de boliche tem peso não inferior a 800g e não superior a 1200g 
    ● bola de basquete tem peso não inferior a 450g e não superior a 600g
    ● bola de gude tem peso não inferior a 5g e não superior a 15g

---
+++ ava-3.php
@@ -0,0 +1,25 @@
+<?php
+	$pbo =$_POST["ipbo"];$sbo =$_POST["isbo"];
+
+	tratamento($pbo,$sbo);
+
+	function tratamento($pbo,$sbo){
+		$a[0] = $pbo;$a[1] = $sbo;
+
+		for ($i=0; $i <= 1; $i++){
+			if(($a[$i] >= 410) && ($a[$i] <= 450)){
+				echo "Bola de futebol oficial <br>";
+			}
+			else if(($a[$i] >= 185) && ($a[$i] <= 240)){
+				echo "Bola de volei oficial <br>";
+			}
+			else if (($a[$i] >= 800) && ($a[$i] <= 1200)){				echo "Bola de boliche oficial <br>";
+			}
+			else if (($a[$i] >= 450) && ($a[$i] <= 600)){				echo "Bola de basquete oficial <br>";
+			}
+			else if (($a[$i] >= 5) && ($a[$i] <= 15)){				
+				echo "Bola de gude oficial <br>";
+			}
+		} 
+	}
+?>
